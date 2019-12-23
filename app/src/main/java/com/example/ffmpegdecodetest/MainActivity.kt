91053b411c3c7f32ea8a6589b766d0651c61bf2d
incus <993624691@qq.com>
2019-12-23 18:40:45 +0800

    nothing

--- app/src/main/java/com/example/ffmpegdecodetest/MainActivity.kt
+++ app/src/main/java/com/example/ffmpegdecodetest/MainActivity.kt
@@ -1,10 +1,25 @@
 package com.example.ffmpegdecodetest
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.zu.ffmpegaudioplayer.data.AudioFile
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    private var songList: ArrayList<AudioFile>? = null
+        set(value) {
+            field = value
+            adapter.data = value
+        }
+
+    private var adapter: SongListAdapter = SongListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +29,57 @@ class MainActivity : AppCompatActivity() {
         btn_print_format.setOnClickListener { nPrintFormatInfo() }
     }
 
+
+    fun listPermissions(): ArrayList<String>
+    {
+        var result = ArrayList<String>()
+        result.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        result.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        return result
+    }
+
+    fun checkPermission()
+    {
+        val permissions = listPermissions()
+        var allGet = true
+        for(permission in permissions)
+        {
+            if(ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
+            {
+                Log.e(TAG, "permission $permission not granted")
+                allGet = false
+            }else{
+                Log.d(TAG, "permission $permission granted")
+            }
+        }
+
+        if(!allGet)
+        {
+            var permissionArray: Array<String> = Array(permissions.size){i: Int -> permissions[i] }
+
+            ActivityCompat.requestPermissions(this, permissionArray, 33)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            33 -> {
+                for(i in grantResults.indices)
+                {
+                    if (grantResults[i] != PackageManager.PERMISSION_GRANTED)
+                    {
+                        Log.e(TAG, "permission ${permissions[i]} not granted")
+                    }else{
+                        Log.d(TAG, "permission ${permissions[i]} granted")
+                    }
+                }
+            }
+        }
+    }
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
---
+++ app/src/main/java/com/example/ffmpegdecodetest/SongListAdapter.kt
@@ -0,0 +1,82 @@
+package com.example.ffmpegdecodetest
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.zu.ffmpegaudioplayer.data.AudioFile
+import java.text.SimpleDateFormat
+
+fun formatDuration(duration: Int): String
+{
+    val totalSeconds: Int = (duration / 1000)
+    val totalMinutes = totalSeconds / 60
+    val seconds: Int = totalSeconds % 60
+    val minutes: Int = totalMinutes % 60
+    val hours: Int = totalMinutes / 60
+
+    var result = "${if (hours == 0) "" else String.format("%02d:", hours)}${String.format("%02d", minutes)}:${String.format("%02d", seconds)}"
+
+
+    return result
+}
+
+
+
+class SongListAdapter: RecyclerView.Adapter<SongListItem>()
+{
+    var data: ArrayList<AudioFile>? = null
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    var itemClickListener: ((position: Int) -> Unit)? = null
+
+    var internalItemClickerListener = object : View.OnClickListener{
+        override fun onClick(v: View?) {
+            if (v == null)
+            {
+                return
+            }
+            itemClickListener?.invoke(v!!.tag as Int)
+        }
+    }
+
+//    var layoutInflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongListItem {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.list_item_song, parent, false)
+        return SongListItem(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data?.size ?: 0
+    }
+
+    override fun onBindViewHolder(holder: SongListItem, position: Int) {
+        if(data == null || position >= data!!.size)
+        {
+            return
+        }
+
+        holder.tvName.text = data!![position].fileName
+        holder.tvDuration.text = formatDuration(data!![position].duration!!)
+        holder.itemView.tag = position
+
+        holder.itemView.setOnClickListener(internalItemClickerListener)
+    }
+}
+
+class SongListItem(itemView: View): RecyclerView.ViewHolder(itemView)
+{
+
+    var tvName: TextView
+    var tvDuration: TextView
+    init {
+        tvName = itemView.findViewById(R.id.tv_name)
+        tvDuration = itemView.findViewById(R.id.tv_duration)
+    }
+}
